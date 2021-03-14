4892fd17ed13271537e17e697ffffb01a3cffabd
iMark <tubagusmakbark@gmail.com>
2021-03-14 20:42:25 +0700

    Main Update tugas
    Main Update tugas

---
+++ W1/Coloring a Moveable Circle/MoveCircle.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MoveCircle {
+    // -----------------------------------
+    // Set up a frame for the GUI.
+    // -----------------------------------
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MoveCircle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(400, 300));
+        frame.getContentPane().add(new CirclePanel(400, 300));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
