11c9e0fcc8ee9a16d3405b1a4df20cc6ce77b604
iMark <tubagusmakbark@gmail.com>
2021-03-15 13:27:49 +0700

    Main Update with netbeans
    halo gan, ini update biar connect netbeans yupps

---
+++ APPL1-PR/src/Week1/Another_Type_of_Employee/Firm.java
@@ -0,0 +1,10 @@
+package Week1.Another_Type_of_Employee;
+public class Firm {
+    // -----------------------------------------------------------------
+    // Creates a staff of employees for a firm and pays them.
+    // -----------------------------------------------------------------
+    public static void main(String[] args) {
+        Staff personnel = new Staff();
+        personnel.payday();
+    }
+}
---
+++ APPL1-PR/src/Week1/Painting_Shapes/PaintThings.java
@@ -0,0 +1,28 @@
+package Week1.Painting_Shapes;
+
+import java.text.DecimalFormat;
+
+public class PaintThings {
+    // -----------------------------------------
+    // Creates some shapes and a Paint object
+    // and prints the amount of paint needed
+    // to paint each shape.
+    // -----------------------------------------
+    public static void main(String[] args) {
+        final double COVERAGE = 350;
+        Paint paint = new Paint(COVERAGE);
+        Rectangle deck;
+        Sphere bigBall;
+        Cylinder tank;
+        double deckAmt, ballAmt, tankAmt;
+        // Instantiate the three shapes to paint
+        // Compute the amount of paint needed for each shape
+
+        // Print the amount of paint for each.
+        DecimalFormat fmt = new DecimalFormat("0.#");
+        System.out.println("\nNumber of gallons of paint needed...");
+        System.out.println("Deck " + fmt.format(deckAmt));
+        System.out.println("Big Ball " + fmt.format(ballAmt));
+        System.out.println("Tank " + fmt.format(tankAmt));
+    }
+}
