b3460cf9384e8e690cd6c05657125e0553a89d7b
iMark <tubagusmakbark@gmail.com>
2021-03-16 19:30:49 +0700

    Moveable Circle done
    Commit case Coloring a Moveable Circle

--- APPL1-PR/src/Week1/Coloring_a_Moveable_Circle/CirclePanel.java
+++ APPL1-PR/src/Week1/Coloring_a_Moveable_Circle/CirclePanel.java
@@ -16,7 +16,7 @@ public class CirclePanel extends JPanel {
         // Set coordinates so circle starts in middle
         x = (width / 2) - (CIRCLE_SIZE / 2);
         y = (height / 2) - (CIRCLE_SIZE / 2);
-        c = Color.green;
+        c = Color.GREEN;
         // Need a border layout to get the buttons on the bottom
         this.setLayout(new BorderLayout());
         // Create buttons to move the circle
@@ -24,11 +24,22 @@ public class CirclePanel extends JPanel {
         JButton right = new JButton("Right");
         JButton up = new JButton("Up");
         JButton down = new JButton("Down");
+        JButton red = new JButton("Red");
+        JButton green = new JButton("Green");
+        JButton blue = new JButton("Blue");
+        JButton custom = new JButton("Custom Color");
         // Add listeners to the buttons
         left.addActionListener(new MoveListener(-20, 0));
         right.addActionListener(new MoveListener(20, 0));
         up.addActionListener(new MoveListener(0, -20));
         down.addActionListener(new MoveListener(0, 20));
+        red.addActionListener(new changeColor(Color.RED));
+        red.setBackground(Color.RED);
+        green.addActionListener(new changeColor(Color.GREEN));
+        green.setBackground(Color.GREEN);
+        blue.addActionListener(new changeColor(Color.BLUE));
+        blue.setBackground(Color.BLUE);
+        custom.addActionListener(new changeColor(null));
         // Need a panel to put the buttons on or they'll be on
         // top of each other.
         JPanel buttonPanel = new JPanel();
@@ -38,6 +49,12 @@ public class CirclePanel extends JPanel {
         buttonPanel.add(down);
         // Add the button panel to the bottom of the main panel
         this.add(buttonPanel, "South");
+        JPanel colorPanel = new JPanel();
+        colorPanel.add(red);
+        colorPanel.add(green);
+        colorPanel.add(custom);
+        colorPanel.add(blue);
+        this.add(colorPanel, "North");
     }
 
     // ---------------------------------------------------------------
@@ -70,6 +87,21 @@ public class CirclePanel extends JPanel {
         public void actionPerformed(ActionEvent e) {
             x += dx;
             y += dy;
+            repaint();
+        }
+    }
+    private class changeColor implements ActionListener{
+        private Color colour;
+        public changeColor(Color colour){
+            this.colour=colour;
+        }
+        public void actionPerformed(ActionEvent e){
+            if(this.colour==null){
+                c=JColorChooser.showDialog(null, "Pick Color", Color.WHITE);
+            }
+            else{
+                c=this.colour;
+            }
             repaint();
         }
     }
--- APPL1-PR/src/Week1/Coloring_a_Moveable_Circle/MoveCircle.java
+++ APPL1-PR/src/Week1/Coloring_a_Moveable_Circle/MoveCircle.java
@@ -10,8 +10,8 @@ public class MoveCircle {
     public static void main(String[] args) {
         JFrame frame = new JFrame("MoveCircle");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(400, 300));
-        frame.getContentPane().add(new CirclePanel(400, 300));
+        frame.setPreferredSize(new Dimension(1280, 720));
+        frame.getContentPane().add(new CirclePanel(1280, 720));
         frame.pack();
         frame.setVisible(true);
     }
